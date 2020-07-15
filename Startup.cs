900972534c92f5a7b7cce4d077727829d21dc059
pxk <puxka_shikof@hotmail.com>
2020-07-14 20:49:35 -0500

    MOdificado UseMySql

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace ApiRest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContextPool<GestionCuentasContext>(options => options.UseLazyLoadingProxies().UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContextPool<GestionCuentasContext>(options => options.UseLazyLoadingProxies().UseMySql("server=localhost;database=gestioncuentas;uid=gestioncuentas;password=gestioncuentas"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
